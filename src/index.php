3f0212773f6815cb739b6d4d464e92ba4d9c63dd
Josemar Martins <josemar21@outlook.pt>
2025-07-21 14:19:12 +0100

    atualizei  design-21

--- src/index.php
+++ src/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Condominio</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <main>
@@ -18,14 +19,36 @@
     
 
     ?>
-    <?php foreach($data as $dado):?>
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Rua</th>
+                <th>Nª da casa</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($data as $dado):?>
+            <tr>
+                <td><?= htmlentities($dado['nome'])?></td>
+                <td><?= htmlentities($dado['rua'])?></td>
+                <td><?= htmlentities($dado['numero_da_casa'])?></td>
+            </tr>
+            <?php endforeach;?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="2">Total de moradores</th>
+                <td>25</td>
+            </tr>
+        </tfoot>
+    </table>
+    
         <p>
-            <?= htmlentities($dado['nome'])?> ||
-            <?= htmlentities($dado['rua'])?> || 
-            <?= htmlentities($dado['numero_da_casa'])?>
+            
         </p>
 
-    <?php endforeach;?>
+   
     </main>
 </body>
 </html>
